6de6ecba225e7d31a77862a98a65ca65e5f9568e
Axel Huizinga <axel@cunity.me>
2019-04-27 23:12:04 +0200

    AutoCommit Build: 0.2.0+154 ok

--- autogit.bat
+++ autogit.bat
@@ -1,9 +1,14 @@
 @echo off
+SetLocal EnableDelayedExpansion
+set CMessage=
+for /F "delims=" %%i in (cmessage.txt) do set CMessage=!CMessage! %%i
 
+echo %CMessage%
+EndLocal
 set /p  Version=<version
 set /p  Build_ID=<build
-set /p CMessage=<cmessage.txt
-echo %CMessage% > .clast
+
+echo %CMessage% > clast.txt
 echo %Build_ID%
 set /A  Build_ID=Build_ID+1
 echo %Build_ID% >build
@@ -12,5 +17,5 @@ echo %Version%.%Build_ID%
 git add -A
 git commit -am "AutoCommit Build: %Version%+%Build_ID% %CMessage%"
 git push
-break>.cmessage
+break>cmessage.txt
 

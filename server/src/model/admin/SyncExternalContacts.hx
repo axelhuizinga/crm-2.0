1a377ccb26e9c549dc88844b90980db1977c808d
axelhuizinga <axel@cunity.me>
2021-01-31 22:12:29 +0100

    improving db synchronization

--- server/src/model/admin/SyncExternalContacts.hx
+++ server/src/model/admin/SyncExternalContacts.hx
@@ -68,7 +68,7 @@ class SyncExternalContacts extends Model
 
 	function getAll():NativeArray {
 		var offset:Int = ( param['offset']>0?param['offset']:0);
-		var sql:String = 'SELECT DISTINCT(client_id) FROM clients ORDER BY client_id';
+		var sql:String = 'SELECT client_id FROM clients ORDER BY client_id';
 		var stmt:PDOStatement = S.syncDbh.query(sql);
 		if(untyped stmt==false)
 		{
@@ -79,9 +79,10 @@ class SyncExternalContacts extends Model
 		{
 			trace(stmt.errorInfo());
 		}
-		var res:NativeArray = (stmt.execute()?stmt.fetchAll(PDO.FETCH_NUM):null);	
+		var res:NativeArray = (stmt.execute()?stmt.fetchAll(PDO.FETCH_COLUMN):null);	
 		
-		trace(Syntax.code("count({0})",res));
+		param['totalRecords'] = Syntax.code("count({0})",res);
+		trace('all:' + param['totalRecords']);
 				
 		//var cleared:Int = S.dbh.exec('TRUNCATE contact_ids');
 		if(S.dbh.errorCode() !='00000')
@@ -91,50 +92,42 @@ class SyncExternalContacts extends Model
 		return res;	
 	}
 
-	function  getMissing() {
-		//var offset:Int = ( param['offset']>0?param['offset']:0);
-		var sql:String = '
-		SELECT DISTINCT(cl.client_id) FROM clients cl 
-INNER JOIN pay_plan pp 
-ON pp.client_id=cl.client_id 
-INNER JOIN pay_source ps 
-ON ps.client_id = cl.client_id;
-WHERE cl.client_id>${offset.int}';
-        var stmt:PDOStatement = S.syncDbh.query(sql);
-		if(untyped stmt==false)
+	function  getMissingClients() {
+
+		var cIDs:Array<Dynamic> = Lib.toHaxeArray(getAll());			
+		trace('got:' +cIDs.length + ' client_ids');
+		//trace('got:' + Syntax.code("count({0})",cIDs) +' client_ids');		
+
+		/*var cleared:Int = S.dbh.exec('DELETE FROM ext_ids WHERE table_name=\'contacts\'');
+		if(S.dbh.errorCode() !='00000')
 		{
-			trace(S.syncDbh.errorInfo());
-			S.sendErrors(dbData, ['getAll query:'=>S.syncDbh.errorInfo()]);
+			trace(S.dbh.errorInfo());var madded:Int = 0;
 		}
-		if(stmt.errorCode() !='00000')
-		{
-			trace(stmt.errorInfo());
+		else 	
+			trace('cleared all contacts IDs from ext_ids');
+
+		for(cid in cIDs){
+			var stored:Int = S.dbh.exec(
+				'INSERT INTO ext_ids VALUES(
+					$cid, ${S.dbQuery.dbUser.id},
+					\'${Type.getClassName(Type.getClass(this)) +'.' + action}\',\'contacts\')');
 		}
-		var res:NativeArray = (stmt.execute()?stmt.fetchAll(PDO.FETCH_NUM):null);	
-		
-		trace(Syntax.code("count({0})",res));
-				
-		//var cleared:Int = S.dbh.exec('TRUNCATE contact_ids');
 		if(S.dbh.errorCode() !='00000')
 		{
 			trace(S.dbh.errorInfo());
 		}
 		else 	
-			trace('cleared all IDs from contact_ids');
-		var cIDs:NativeArray = Syntax.code("array_map(function($r){return $r[0];}, {0})",res);
-		
-		trace(Syntax.code("print_r({0}[0],1)", cIDs));
-		var IDs = Syntax.code("implode(',',{0})", cIDs);
-
-		sql = comment(unindent, format)/**
-		SELECT ARRAY_TO_STRING(array_agg(cid.id),',') from contact_ids cid
+			trace('stored all client_ids to ext_ids');
+*/
+		var sql:String = comment(unindent, format)/**
+		SELECT ARRAY_TO_STRING(array_agg(cid.ext_id),',') from ext_ids cid
 		left join 
 		(SELECT 1 as gg,id from contacts) c
-		ON cid.id=c.id
+		ON cid.ext_id=c.id
 		where c.id IS NULL
 		GROUP BY gg;
 		**/;
-		stmt = S.dbh.query(sql);
+		var stmt:PDOStatement = S.dbh.query(sql);
 		if(untyped stmt==false)
 		{
 			trace('$sql ${Std.parseInt(param['limit'])}');
@@ -145,61 +138,49 @@ WHERE cl.client_id>${offset.int}';
 			trace(stmt.errorInfo());
 		}
 		var ids:String = (stmt.execute()?stmt.fetch(PDO.FETCH_COLUMN,0):null);
-		///trace('missing:' + count(explode(',',ids)));
-		sql = comment(unindent,format)/*
-		SELECT cl.client_id id,cl.lead_id,cl.creation_date,cl.state,cl.use_email,cl.register_on,cl.register_off,cl.register_off_to,cl.teilnahme_beginn,cl.title title_pro,cl.anrede title,cl.namenszusatz,cl.co_field,cl.storno_grund,IF(TO_DAYS(STR_TO_DATE(cl.birth_date, '%Y-%m-%d'))>500000 ,cl.birth_date,null) date_of_birth,IF(cl.old_active=1,'true','false')old_active,
-pp.pay_plan_id,pp.creation_date,pp.pay_source_id,pp.target_id,pp.start_day,pp.start_date,pp.buchungs_tag,pp.cycle,pp.amount,IF(pp.product='K',2,3) product ,pp.agent,pp.agency_project project,pp.pay_plan_state,pp.pay_method,pp.end_date,pp.end_reason,pp.repeat_date,
- ps.pay_source_id,ps.debtor,ps.bank_name,ps.account,ps.blz,ps.iban,ps.sign_date,ps.pay_source_state,ps.creation_date account_creation_date,
-vl.entry_date,vl.modify_date,vl.status,vl.user,vl.source_id,vl.list_id,vl.phone_code,vl.phone_number,'' fax,vl.first_name,vl.last_name,vl.address1 address,vl.address2 address_2,vl.city,vl.postal_code,vl.country_code,IF(vl.gender='U','',vl.gender) gender,
-IF( vl.alt_phone LIKE '1%',vl.alt_phone,'')mobile,vl.email,vl.comments,vl.last_local_call_time,vl.owner,vl.entry_list_id, 1 mandator, 100 edited_by, '' company_name
-FROM clients cl
-INNER JOIN pay_plan pp
-ON pp.client_id=cl.client_id
-INNER JOIN pay_source ps
-ON ps.client_id=cl.client_id
-INNER JOIN asterisk.vicidial_list vl
-ON (vl.lead_id=cl.lead_id )
-WHERE cl.client_id IN($ids)
-ORDER BY cl.client_id  
-${limit.sql}
-*/;
-
-        stmt = S.syncDbh.query(sql);
-		if(untyped stmt==false)
-		{
-			trace(sql);
-			S.sendErrors(dbData, ['getMissingCrmData query:'=>S.syncDbh.errorInfo()]);
+		var missing_client_ids:Array<Dynamic> = ids.split(',');
+		trace(param['totalRecords'] + ' - missing:' + missing_client_ids.length);
+		var madded:Int = 0;
+		while(offset.int<param['totalRecords']){
+			var cData:Array<NativeArray> = cast( Lib.toHaxeArray(getCrmClients()));
+			var _1st:Bool = true;
+			trace(cData.length);
+			//for(mcid in missing_client_ids)
+			for(row in cData)
+			{			
+				if(_1st)
+					trace(row);
+				//S.sendErrors(dbData,['importContacts'=>'NOTOK']);
+				if(!missing_client_ids.contains(Std.parseInt(row['id'])))
+					continue;
+				var stmt:PDOStatement = upsertClient(row);
+				try{
+					var res:NativeArray = stmt.fetchAll(PDO.FETCH_ASSOC);		
+					if(_1st){
+						_1st=false;
+						trace(row);
+						trace(res);
+						//Sys.exit(333);
+					}
+				}
+				catch(e:Dynamic)
+				{
+					{S.sendErrors(dbData, [
+						'dbError'=>S.dbh.errorInfo(),
+						'upsertClient'=>S.errorInfo(row),
+						'exception'=>e
+					]);}		
+				}
+				madded++;
+			}		
+			trace(offset.int);
 		}
-		if(stmt.errorCode() !='00000')
-		{
-			S.sendErrors(dbData,['getMissingCrmData'=>stmt.errorInfo()]);
-		}
-		res = (stmt.execute()?stmt.fetchAll(PDO.FETCH_ASSOC):null);
-		var missing:Int = Syntax.code("count({0})",res);
-		trace(missing);
-		for(row in res.iterator())
-		{			
-			//trace(row);
-			//S.sendErrors(dbData,['importContacts'=>'NOTOK']);
-			var stmt:PDOStatement = upsertClient(row);
-			try{
-				var res:NativeArray = stmt.fetchAll(PDO.FETCH_ASSOC);		
-				//trace(res);
-			}
-			catch(e:Dynamic)
-			{
-				{S.sendErrors(dbData, [
-					'dbError'=>S.dbh.errorInfo(),
-					'upsertClient'=>S.errorInfo(row),
-					'exception'=>e
-				]);}		
-			}
-		}		
-        trace('done');
+        trace('done - added ${madded}');
 		//dbData.dataInfo['offset'] = param['offset'] + synced;
 		trace(dbData.dataInfo);
         //S.sendData(dbData, null);
-		S.sendInfo(dbData,['gotMissing'=>'OK:$missing']);
+		S.sendInfo(dbData,['gotMissing'=>'OK:${missing_client_ids.length}']);
+		Sys.exit(0);
 	}
 
 	function syncImportDeals() {
@@ -418,7 +399,9 @@ ${limit.sql}
     //inline function testValue(v:Dynamic):Bool return cast v;
 
     function upsertClient(rD:NativeArray):PDOStatement
-    {
+    {		
+		//trace(rD);
+		//trace(Syntax.code("implode({0})",rD));
 		var cD:Map<String,Dynamic> = Util.map2fields(rD, keys);
         var cNames:Array<String> = [for(k in cD.keys()) k];
 		//var cVals:String =  [for(v in cD.iterator()) v].map(function (v) return '\'$v\'').join(',');
@@ -433,7 +416,8 @@ ${limit.sql}
 			ON CONFLICT (id) DO UPDATE
 			SET $cSet returning id;
 		*/;
-		//trace(sql);
+		trace(sql);
+		//Sys.exit(333);
 		var stmt:PDOStatement = S.dbh.prepare(sql,Syntax.array(null));
 		Util.bindClientData('contacts',stmt,rD,dbData);
 		try{
@@ -494,8 +478,7 @@ ${limit.sql}
 		trace('offset:${param['offset']}::${offset.sql}<<<');
 
 		var sql = comment(unindent,format)/*
-SELECT * FROM clients
-ORDER BY client_id 
+SELECT cl.client_id id, cl.* FROM fly_crm.clients cl 
 ${limit.sql} ${offset.sql}
 */;
 		trace('$sql');
@@ -520,6 +503,9 @@ ${limit.sql} ${offset.sql}
 			dbData.dataInfo['totalRecords'] = totalRes;
 			trace(dbData.dataInfo);
 		}
+
+		offset = Util.offset(offset.int + Syntax.code("array_count({0})",res));
+
 		return res;
 	}
 	
